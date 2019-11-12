92421d651a775a7d82ef6aeca398cde4acaf39a6
Kenny He <khe618@yahoo.com>
2019-11-12 16:46:18 -0600

    test

--- app.js
+++ app.js
@@ -18,6 +18,13 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
+app.get("/events", function (req, res){
+	db.collection("events").findOne({}, function(err, result){
+		if (err) throw err;
+		res.json(result)
+	})
+})
+
 
 MongoClient.connect("mongodb://heroku_4bgzbp8r:srsgfdtlepejihdfa2ruarggr5@ds053937.mlab.com:53937/heroku_4bgzbp8r", function (err, client){
 	db = client.db("heroku_4bgzbp8r")
