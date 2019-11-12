9d972da0c25b23cb5e850240aeb0c56a2e1b5228
Kenny He <khe618@yahoo.com>
2019-11-12 16:34:59 -0600

    test

--- app.js
+++ app.js
@@ -19,8 +19,7 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 
-MongoClient.connect("mongodb://heroku_27t6lj7p:o0sscid6eke2jcn6009s68o30u@ds115762.mlab.com:15762/heroku_27t6lj7p", function (err, client){
-	db = client.db("heroku_27t6lj7p")
-	app.listen(process.env.PORT || 5000);
-	
+MongoClient.connect("mongodb://heroku_4bgzbp8r:srsgfdtlepejihdfa2ruarggr5@ds053937.mlab.com:53937/heroku_4bgzbp8r", function (err, client){
+	db = client.db("heroku_4bgzbp8r")
+	app.listen(process.env.PORT || 5000);	
 })
