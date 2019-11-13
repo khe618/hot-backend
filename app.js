016d68233db3f52cafa0b3dc2a774c63c0bd8006
Kenny He <khe618@yahoo.com>
2019-11-12 23:07:09 -0600

    test

--- app.js
+++ app.js
@@ -18,33 +18,52 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-app.get("/events/:eventId([0-9A-Za-z]*)", function (req, res){
+app.get("/events/:eventId([0-9A-Za-z]*)", async function (req, res){
 	var eventId = req.params.eventId;
 	var query = {_id: ObjectId(eventId)}
-	db.collection("events").findOne(query, function(err, result){
-		if (err) throw err;
-		res.json(result)
-	})
+	result = await db.collection("events").findOne(query)
+	return result;
 })
 
-app.delete("/events/:eventId([0-9A-Za-z]*)", function (req, res){
+app.delete("/events/:eventId([0-9A-Za-z]*)", async function (req, res){
 	var eventId = req.params.eventId;
 	var query = {_id: ObjectId(eventId)}
-	db.collection("events").deleteOne(query, function(err, result){
-		if (err) throw err;
-		res.json(result)
-	})
+	await db.collection("events").deleteOne(query)
+	res.sendStatus(200);
 })
 
-app.post("/events", function(req, res){
+app.post("/events", async function(req, res){
+	var data = req.body;
+	await db.collection("events").insertOne(data)
+	res.sendStatus(200)
+})
+
+app.get("/users/:userId([0-9A-Za-z]*)", async function (req, res){
+	var userId = req.params.userId;
+	var query = {_id: ObjectId(userId)}
+	result = await db.collection("users").findOne(query)
+	res.json(result)
+})
+
+app.delete("/users/:userId([0-9A-Za-z]*)", async function (req, res){
+	var userId = req.params.userId;
+	var query = {_id: ObjectId(userId)}
+	await db.collection("users").deleteOne(query)
+	res.sendStatus(200)
+})
+
+app.post("/users", async function(req, res){
 	var data = req.body
-	db.collection("events").insertOne(data, function(err, result){
-		if(err) throw err;
-		else{
-			res.sendStatus(200)
-		}
-	})
+	db.collection("users").insertOne(data)
+	res.sendStatus(200)
 })
+
+app.get("/users-events/:userId([0-9A-Za-z]*)", async function(req, res){
+	var userId = req.params.userId;
+	var query = {_id:ObjectId(userId)}
+
+})
+
 
 MongoClient.connect("mongodb://heroku_4bgzbp8r:srsgfdtlepejihdfa2ruarggr5@ds053937.mlab.com:53937/heroku_4bgzbp8r", function (err, client){
 	db = client.db("heroku_4bgzbp8r")
