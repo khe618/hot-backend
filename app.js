873b3d48a1d2eacbecae9dd3b6e05eff6ddeba4c
Kenny He <khe618@yahoo.com>
2019-11-13 16:17:34 -0600

    test

--- app.js
+++ app.js
@@ -23,6 +23,26 @@ const asyncMiddleware = fn =>
       .catch(next);
   };
 
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
+}
+
+function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);
+  var dLon = deg2rad(lon2-lon1); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ; 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c; // Distance in km
+  return d;
+}
+
+
+
 
 // -- setup up swagger-jsdoc --
 const swaggerDefinition = {
@@ -197,6 +217,15 @@ app.get("/usersEvents/events/:eventId([0-9a-f]{24})/:status", asyncMiddleware(as
     var userIds = userEvents.map(e => ObjectId(e.userId))
     var userObjects = await db.collection("users").find({_id: {$in: userIds}}).toArray()
     res.json(userObjects)
+}))
+
+app.get("/nearestEvents", asyncMiddleware(async (req, res, next) => {
+    var latitude = req.query.latitude;
+    var longitude = req.query.longitude;
+    var result = await db.collection("events").find({}).toArray()
+    result.sort((a, b) => getDistanceFromLatLonInKm(latitude, longitude, a.latitude, a.longitude) - 
+        getDistanceFromLatLonInKm(latitude, longitude, b.latitude, b.longitude))
+    res.json(result)
 }))
 
 
